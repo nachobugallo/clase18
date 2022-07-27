09bab58cbd87c6d8a990dc4c0ced14c1c2e450d9
Ignacio Bugallo <ignacio.bugallo@gmail.com>
2022-07-26 21:15:03 -0300

    actualizacion

--- Ejercicio18/Ejercicio18/views.py
+++ Ejercicio18/Ejercicio18/views.py
@@ -5,7 +5,7 @@ from Ejercicio18.models import Perfil
 
 
 def Perfiles(request):
-    users1 = Perfil.objects.create(name="Pedro", wager=75000, creation_date=25/7/2022)
-    users2 = Perfil.objects.create(name="Juan", wager=50000, creation_date=25/7/2022)
-    users3 = Perfil.objects.create(name="Jose", wager=79000, creation_date=25/7/2022)
+    users1 = Perfil.objects.create(name="Pedro", wager=75000, creation_date=("2002-9-22"))
+    users2 = Perfil.objects.create(name="Juan", wager=50000, creation_date=("2005-9-15"))
+    users3 = Perfil.objects.create(name="Jose", wager=79000, creation_date=("2012-3-5"))
     return render(request, "MVT.html", context= [users1, users2, users3])
